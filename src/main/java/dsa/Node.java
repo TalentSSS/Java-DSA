2cbc3e51be11cf7ddb71098404ebfe448ed3337c
TalentSSS <516282141@qq.com>
2019-05-07 00:03:14 +0800

    修改注释

--- src/main/java/dsa/Node.java
+++ src/main/java/dsa/Node.java
@@ -13,7 +13,6 @@ public class Node implements Position {
      */
     private Node next;
 
-    /********************构造方法********************/
     public Node() {
         this(null, null);
     }
@@ -23,25 +22,40 @@ public class Node implements Position {
         next = n;
     }
 
-    /********************Position接口方法********************/
+    /**
+     * 返回存放于该位置的数据
+     * @return 该位置的元素
+     */
     @Override
     public Object getElem() {
         return element;
     }
 
+    /**
+     * 将给定元素存放于该位置，返回之前存放的元素
+     * @param e 给定的元素
+     * @return 之前存放的元素
+     */
     @Override
     public Object setElem(Object e) {
         Object oldElem = element;
         element = e;
         return oldElem;
     }
-    /********************单链表节点方法********************/
+
+    /**
+     * 找到后继节点
+     * @return 后继节点
+     */
     public Node getNext() {
         return next;
     }
 
+    /**
+     * 修改后继节点
+     * @param newNext 新的后继节点
+     */
     public void setNext(Node newNext) {
         next = newNext;
     }
-
 }
--- src/main/java/dsa/Position.java
+++ src/main/java/dsa/Position.java
@@ -7,14 +7,14 @@ public interface Position {
 
     /**
      * 返回存放于该位置的数据
-     * @return
+     * @return 该位置的元素
      */
     Object getElem();
 
     /**
      * 将给定元素存放于该位置，返回之前存放的元素
      * @param e 给定的元素
-     * @return Object 之前存放的元素
+     * @return 之前存放的元素
      */
     Object setElem(Object e);
 }
